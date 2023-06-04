9a8b46c96a5f7faa1fe10a95e79c309bd64ae56c
Chris Wilson <chrisw@myself.com>
2023-06-04 09:42:23 -0400

    wrapped a try/catch around startup logic to narrow the problem

--- server/server.js
+++ server/server.js
@@ -19,25 +19,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/public")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-const startServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
+const startServer = async () => {
+  try {
+    await server.start();
+    server.applyMiddleware({ app });
 
-  db.once("open", () => {
+    await db.once("open");
+
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
         `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
       );
     });
-  });
+  } catch (error) {
+    console.error("An error occurred while starting the server:");
+    console.error(error);
+    process.exit(1);
+  }
 };
 
-startServer(typeDefs, resolvers);
+startServer();
