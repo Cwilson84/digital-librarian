81b08d2b1bb86390e8dac8106e33dcbd717293fe
Chris Wilson <chrisw@myself.com>
2023-06-06 11:26:43 -0400

    minor change to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/digital-librarian')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/digital-librarian');
 
 module.exports = mongoose.connection;
